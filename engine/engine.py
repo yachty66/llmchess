ae8fca66b0b67e0e643003904fb7d6808842d906
Max Hager <maxhager28@gmail.com>
2023-03-22 18:51:06 -0700

    update request method

--- engine/engine.py
+++ engine/engine.py
@@ -27,7 +27,6 @@ class ChessEngine:
             }
         ]
 
-    #create a method which checks if the move is legal
     def is_legal_move(self, move):
         try:
             print(self.board)
@@ -53,27 +52,14 @@ class ChessEngine:
                     response = self.get_gpt_response(self.messages)
                     move = response.split("Best move:")[-1].strip().split()[0]
             return self.board.uci(chess.Move.from_uci(self.board.move_stack[1].uci()))
-        """
-        - [x] make beginning always working
-        - [x] start loop 
-        - [x] convert move to san
-        - [x] make request with new move
-        - [x] check if move is valid  
-        - [x] if valid return move in uci and add to board
-        - [x] if not print something and repeat
-        - [ ] find out why stop with responding
-        """
         san_move = self.board.san(chess.Move.from_uci(move_from + move_to))
         self.messages.append({"role": "user", "content": f"{san_move}"})
         self.board.push_san(san_move)
         while True:
+            print("next round")
             response = self.get_gpt_response(self.messages)
-            print("20*-")
-            print(response)
-            #get move from response
+            print("response",response)
             move = response.split("Best move:")[-1].strip().split()[0]
-            #validate move
-            #first need to add move to board
             if self.is_legal_move(move):
                 self.messages.append({"role": "assistant", "content": response})
                 uci_move = self.board.peek().uci()
@@ -84,18 +70,15 @@ class ChessEngine:
 
     def get_gpt_response(self, messages):
         url = "https://api.openai.com/v1/chat/completions"
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bearer " + OPENAI_API_KEY,
-        }
-        payload = {
-            "model": "gpt-3.5-turbo",
-            "messages": messages,
-            "temperature": 0.7,
-        }
-        response = requests.post(url, headers=headers, data=json.dumps(payload))
-        response_json = response.json()
-        return response_json["choices"][0]["message"]["content"].strip()
+        print("messages", messages)
+
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+        )
+        print("completion")
+        print(completion.choices[0].message["content"].strip())
+        return completion.choices[0].message["content"].strip()
     
 
 
